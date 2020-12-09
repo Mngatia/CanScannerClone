281e55ba3b09f24ffaad7789d943d7b1b62b2480
Mngatia <nyawirahngatia@gmail.com>
2020-12-09 23:02:31 +0300

    Converted image to pdf.

--- app/src/main/java/com/example/canscannerclone/ScanActivity.java
+++ app/src/main/java/com/example/canscannerclone/ScanActivity.java
@@ -3,10 +3,12 @@ package com.example.canscannerclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -18,9 +20,18 @@ import com.example.canscannerclone.Retrofit.RetrofitClient;
 import com.example.canscannerclone.Utils.Common;
 import com.example.canscannerclone.Utils.IUploadCallbacks;
 import com.example.canscannerclone.Utils.ProgressRequestBody;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
 import okhttp3.MultipartBody;
@@ -37,7 +48,8 @@ public class ScanActivity extends AppCompatActivity implements IUploadCallbacks 
     //ProgressBar dialog;
     ProgressDialog dialog;
 
-    public String Image_name="";
+    //One string to save image name
+    public String image_name="";
 
 
     BitmapDrawable bitmapDrawable;
@@ -67,6 +79,88 @@ public class ScanActivity extends AppCompatActivity implements IUploadCallbacks 
         Uri image_uri = getIntent().getData();
         imageView.setImageURI(image_uri);
         selectedFileUri = image_uri;  //uri for image that we take from camera
+
+        //cancel and save function
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //on click cancel we will redirect to home activity without saving any image
+                Intent i =new Intent(ScanActivity.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bitmapDrawable = (BitmapDrawable)imageView.getDrawable();
+                bitmap = bitmapDrawable.getBitmap();
+
+                FileOutputStream outputStream= null;
+
+                //path to save file
+                File sdCard = Environment.getExternalStorageDirectory();
+                File directory = new File(sdCard.getAbsolutePath()+"/CanScannerCloneStorage");
+                directory.mkdir();
+                //directory with the name CanScannerCloneStorage
+                String fileName = image_name+".jpg";
+                File outFile = new File(directory,fileName);
+
+                try {
+                    outputStream = new FileOutputStream(outFile);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100,outputStream);
+                    outputStream.flush();
+                    outputStream.close();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                Document document =  new Document();
+
+                String directoryPath = android.os.Environment.getExternalStorageDirectory().getAbsolutePath()+"/CanScannerCloneStorage";
+
+                try {
+                    PdfWriter.getInstance(document,new FileOutputStream(directoryPath +"/"+ image_name+".pdf"));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (DocumentException e) {
+                    e.printStackTrace();
+                }
+
+                document.open();
+
+                Image image = null;
+                try {
+                    image = Image.getInstance(directoryPath+"/"+image_name+".jpg");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (BadElementException e) {
+                    e.printStackTrace();
+                }
+                float scaler = ((document.getPageSize().getWidth() - document.leftMargin() - document.rightMargin() -0) /image.getWidth())*100;
+                //0 means you have no indentation
+
+                image.scalePercent(scaler);
+                image.setAlignment(Image.ALIGN_CENTER | Image.ALIGN_TOP);
+
+                try {
+                    document.add(image);
+                } catch (DocumentException e) {
+                    e.printStackTrace();
+                }
+                document.close();
+                Toast.makeText(ScanActivity.this, "PDF Saved Successfully..", Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(ScanActivity.this,HomeActivity.class);
+                startActivity(i);
+
+                //This point we create a directory path read image with their name and open pdf and write in pdf
+                //After pdf saved successfully redirect to home where we can see saved pdf
+
+            }
+        });
 
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,7 +217,16 @@ public class ScanActivity extends AppCompatActivity implements IUploadCallbacks 
                                                 .rotate(90)
                                                 .into(imageView);   //set image into image view
 
-                                        //after loading dismiss dialog
+                                        //After loading image to image view, we have to
+
+                                        image_name += response.body().replace("\"", "").split("/")[1];
+
+                                        btnUpload.setVisibility(View.INVISIBLE);
+                                        Save.setVisibility(View.VISIBLE);
+                                        Cancel.setVisibility(View.VISIBLE);
+
+                                        //we can ave and cancel image
+                                        //We ned image name to save image name from response.body
 
                                         dialog.dismiss();
 
